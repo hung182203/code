6c1010a98e72ab4d9de6a2ef27b43ee7a3bc2eab
hung182203 <163118764+hung182203@users.noreply.github.com>
2024-12-14 08:48:23 +0700

    Update aodv50.tcl

--- aodv50.tcl
+++ aodv50.tcl
@@ -1,124 +1,115 @@
-Mac/802_11 set dataRate_ 11Mb ;#Rate for Data Frames
-set val(chan) Channel/WirelessChannel ;# channel type
-set val(prop) Propagation/TwoRayGround ;# radio-propagation model
-set val(ant) Antenna/OmniAntenna ;# Antenna type
-set val(ll) LL ;# Link layer type
-set val(ifq) Queue/DropTail/PriQueue ;# Interface queue type
-set val(ifqlen) 50 ;# max packet in ifq
-set val(netif) Phy/WirelessPhy ;# network interface type
-set val(mac) Mac/802_11 ;# MAC type
-set val(nn) 50 ;# number of mobilenodes
-set val(rp) AODV ;# routing protocol
-set val(x) 10000
-set val(y) 10000
+# AODV Routing Protocol Simulation
+# Mô phỏng mạng MANET với 50 nút
+
+# Cấu hình mạng không dây
+set val(chan) Channel/WirelessChannel     ;# Loại kênh
+set val(prop) Propagation/TwoRayGround    ;# Mô hình lan truyền
+set val(ant) Antenna/OmniAntenna          ;# Loại anten
+set val(ll) LL                            ;# Lớp liên kết
+set val(ifq) Queue/DropTail/PriQueue      ;# Loại hàng đợi
+set val(ifqlen) 50                        ;# Độ dài tối đa hàng đợi
+set val(netif) Phy/WirelessPhy            ;# Loại giao diện mạng
+set val(mac) Mac/802_11                   ;# Loại MAC
+set val(rp) AODV                          ;# Giao thức định tuyến
+set val(nn) 50                            ;# Số lượng nút
+set val(x) 1000                           ;# Chiều rộng địa hình
+set val(y) 1000                           ;# Chiều cao địa hình
+
+# Tạo đối tượng Simulator
 set ns [new Simulator]
-#ns-random 0
-set tracefd [open aodv50.tr w]
-#set windowVsTime2 [open win.tr w]
-set namtrace [open aodv50.nam w]
+
+# Thiết lập truy vết
+set tracefd [open aodv_simulation.tr w]
+set namtrace [open aodv_simulation.nam w]
+
+# Sử dụng truy vết mới
 $ns use-newtrace
 $ns trace-all $tracefd
 $ns namtrace-all-wireless $namtrace $val(x) $val(y)
-#set f0 [open So_Goi_Da_Gui.tr w]
-#set f1 [open So_Goi_Mat.tr w]
-#set f2 [open proj_out2.tr w]
-#set f3 [open proj_out3.tr w]
+
+# Tạo địa hình
 set topo [new Topography]
 $topo load_flatgrid $val(x) $val(y)
-create-god $val(nn)
-set chan_1 [new $val(chan)]
 
-# CONFIGURE AND CREATE NODES
+# Tạo God (Global Oracle of Destiny) để quản lý mạng không dây
+create-god $val(nn)
+
+# Tạo kênh không dây
+set chan [new $val(chan)]
+
+# Cấu hình nút
 $ns node-config -adhocRouting $val(rp) \
--llType $val(ll) \
--macType $val(mac) \
--ifqType $val(ifq) \
--ifqLen $val(ifqlen) \
--antType $val(ant) \
--propType $val(prop) \
--phyType $val(netif) \
-#-channelType $val(chan) \
--topoInstance $topo \
--agentTrace OFF \
--routerTrace ON \
--macTrace ON \
--movementTrace OFF \
--channel $chan_1 # \
-proc finish {} {
-global ns f f0 f1 f2 f3 namtrace
-$ns flush-trace
-close $namtrace
-exec nam -r 5m aodv50.nam &
-exit 0
-}
-proc record {} {
-global sink0 sink1 sink2 sink3 sink4 sink5 sink6 sink7 sink8 sink9 f0 f1 f2 f3
-#Get An Instance Of The Simulator
-set ns [Simulator instance]
-#Set The Time After Which The Procedure Should Be Called Again
-set time 0.05
-#How Many Bytes Have Been Received By The Traffic Sinks?
-set bw0 [$sink9 set npkts_]
-set bw1 [$sink9 set nlost_]
-#set bw2 [$sink2 set npkts_]
-#set bw3 [$sink3 set npkts_]
-#Get The Current Time
-set now [$ns now]
-#Save Data To The Files
-puts $f0 "$now [expr $bw0]"
-puts $f1 "$now [expr $bw1]"
-#puts $f2 "$now [expr $bw2]"
-#puts $f3 "$now [expr $bw3]"
-#Re-Schedule The Procedure
-$ns at [expr $now+$time] "record"
-}
+                -llType $val(ll) \
+                -macType $val(mac) \
+                -ifqType $val(ifq) \
+                -ifqLen $val(ifqlen) \
+                -antType $val(ant) \
+                -propType $val(prop) \
+                -phyType $val(netif) \
+                -channelType $val(chan) \
+                -topoInstance $topo \
+                -agentTrace ON \
+                -routerTrace ON \
+                -macTrace ON \
+                -movementTrace ON \
+                -channel $chan
+
+# Tạo các nút
 for {set i 0} {$i < $val(nn)} {incr i} {
-set n_($i) [$ns node]
-$n_($i) random-motion 0
-$ns initial_node_pos $n_($i) 20
+    set node($i) [$ns node]
+    $node($i) random-motion 0
 }
-for {set i 0} {[expr $i < $val(nn)/10]} {incr i} {
-for {set j 0} {$j < 10} {incr j} {
-set id [expr $i*10 + $j]
-$n_($id) set X_ [expr $j*100+120]
-$n_($id) set Y_ [expr $i*100+120]
-$n_($id) set Z_ 0.0
-}
-}
+
+# Đặt vị trí ban đầu cho các nút
 for {set i 0} {$i < $val(nn)} {incr i} {
-if {$i%3 == 0} {
-$ns at 0.0 "$n_($i) setdest [expr $i%7*1040+20] [expr $i%3*3000+20] 10.0"
+    $node($i) set X_ [expr rand() * $val(x)]
+    $node($i) set Y_ [expr rand() * $val(y)]
+    $node($i) set Z_ 0.0
 }
-if {$i%3 == 1} {
-$ns at 0.0 "$n_($i) setdest [expr $i%6+20] [expr $i%3*3000+20] 10.0"
-}
-if {$i%3 == 2} {
-$ns at 0.0 "$n_($i) setdest [expr $i%7*1040+20] [expr $i%3+10] 10.0"
-}
-}
-# CONFIGURE AND SET UP A FLOW
+
+# Định nghĩa mô hình di chuyển
 for {set i 0} {$i < $val(nn)} {incr i} {
-set sink_($i) [new Agent/LossMonitor]
-$ns attach-agent $n_($i) $sink_($i)
-set tcp_($i) [new Agent/TCP]
-$ns attach-agent $n_(0) $tcp_($i)
+    $ns at 0.0 "$node($i) setdest [expr rand()*$val(x)] [expr rand()*$val(y)] [expr 10+rand()*20]"
 }
-proc attach-CBR-traffic { node sink size interval } {
-#Get an instance of the simulator
-set ns [Simulator instance]
-#Create a CBR agent and attach it to the node
+
+# Tạo các luồng lưu lượng
+# Luồng CBR từ nút nguồn tới nút đích
+set src_node $node(0)
+set dst_node $node([expr $val(nn)-1])
+
+# Tạo sink (điểm nhận)
+set sink [new Agent/LossMonitor]
+$ns attach-agent $dst_node $sink
+
+# Tạo nguồn CBR
 set cbr [new Agent/CBR]
-$ns attach-agent $node $cbr
-$cbr set packetSize_ $size
-$cbr set interval_ $interval
-#Attach CBR source to sink;
+$cbr set packetSize_ 512
+$cbr set interval_ 0.25
+$ns attach-agent $src_node $cbr
 $ns connect $cbr $sink
-return $cbr
+
+# Lập lịch cho các sự kiện
+$ns at 0.0 "$cbr start"
+$ns at 30.0 "$cbr stop"
+
+# Hàm kết thúc mô phỏng
+proc finish {} {
+    global ns tracefd namtrace
+    $ns flush-trace
+    close $tracefd
+    close $namtrace
+    
+    # Chạy Nam để trực quan hóa (không bắt buộc)
+    exec nam aodv_simulation.nam &
+    exit 0
 }
-set cbr0 [attach-CBR-traffic $n_(0) $sink_([expr $val(nn)-1]) 512 0.25]
-set cbr0 [attach-CBR-traffic $n_(3) $sink_([expr $val(nn)-2]) 512 0.25]
-$ns at 0.0 "$cbr0 start"
-$ns at 30.0 "$cbr0 stop"
-$ns at 30.0 "finish"
-puts "Start of simulation.."
+
+# Đặt sự kiện kết thúc
+$ns at 31.0 "finish"
+
+# Thiết lập seed ngẫu nhiên (tùy chọn)
+ns-random 0
+
+# Chạy mô phỏng
+puts "Bắt đầu mô phỏng AODV..."
 $ns run
